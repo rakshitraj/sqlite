cfd57ceea43e008e2533a9aa2ae09a74a1a3f94c
Rakshit <hello@rakshitraj.com>
2023-07-13 16:01:43 +0530

    feat: use memcpy for ID

--- db.c
+++ db.c
@@ -322,13 +322,13 @@ void db_close(Table* table) {
 // }
 
 void serialize_row(Row* source, void* destination) {
-    strncpy(destination + ID_OFFSET, source->id, ID_SIZE);
+    memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
     strncpy(destination + USERNAME_OFFSET, source->username, USERNAME_SIZE);
     strncpy(destination + EMAIL_OFFSET, source->email, EMAIL_SIZE);
 }
 
 void deserialize_row(void* source, Row* destination) {
-    strncpy(destination->id, source + ID_OFFSET, ID_SIZE);
+    memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
     strncpy(destination->username, source + USERNAME_OFFSET, USERNAME_SIZE);
     strncpy(destination->email, source + EMAIL_OFFSET, EMAIL_SIZE);
 }
