f4387dc244ac6a8ad40bd2ce06f5ef2407fc698e
Rakshit <hello@rakshitraj.com>
2023-09-26 22:57:44 +0530

    btree single leaf node

--- db.c
+++ db.c
@@ -260,6 +260,71 @@ void pager_flush(Pager* pager, uint32_t page_num) {
 }
 
 
+// Database bloc
+
+typedef enum {
+    NODE_INTERNAL,
+    NODE_LEAF
+} NodeType;
+
+/*
+ * Common node Header Layout 
+ * 
+ * Nodes need to store some metadata in the header at the be=ginning of every page. theis information is common across all nodes.
+ * Every node will store the type of node is, weather or not it is the root node, and the address of it parent so that we can find its sibling nodes.
+ * The offset caused due to storing this information is calculated and reference below. 
+ */
+const uint32_t NODE_TYPE_SIZE = sizeof(uint8_t);
+const uint32_t NODE_TYPE_OFFSET = 0;
+const uint32_t IS_ROOT_SIZE = sizeof(uint8_t);
+const uint32_t IS_ROOT_OFFSET = NODE_TYPE_SIZE;
+const uint32_t PARENT_POINTER_SIZE = sizeof(uint32_t);
+const uint32_t PARENT_POINTER_OFFSET = IS_ROOT_OFFSET + IS_ROOT_SIZE;
+const uint8_t COMMON_NODE_HEADER_SIZE = NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_POINTER_SIZE;
+
+/*
+* Leaf Node Header Layout
+*/
+const uint32_t LEAF_NODE_NUM_CELLS_SIZE = sizeof(uint32_t);
+const uint32_t LEAF_NODE_NUM_CELLS_OFFSET = COMMON_NODE_HEADER_SIZE;
+const uint32_t LEAF_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE + LEAF_NODE_NUM_CELLS_SIZE;
+
+/*
+* Leaf Node Body Layout
+*/
+const uint32_t LEAF_NODE_KEY_SIZE = sizeof(uint32_t);
+const uint32_t LEAF_NODE_KEY_OFFSET = 0;
+const uint32_t LEAF_NODE_VALUE_SIZE = ROW_SIZE;
+const uint32_t LEAF_NODE_VALUE_OFFSET = LEAF_NODE_KEY_OFFSET + LEAF_NODE_KEY_SIZE;
+const uint32_t LEAF_NODE_CELL_SIZE = LEAF_NODE_KEY_SIZE + LEAF_NODE_VALUE_SIZE;
+const uint32_t LEAF_NODE_SPACE_FOR_CELLS = PAGE_SIZE - LEAF_NODE_HEADER_SIZE;
+const uint32_t LEAF_NODE_MAX_CELLS = LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE;
+
+// Accessing leaf node fields
+// functions return a pointer, thus they can be used as both getter and setter
+
+uint32_t* leaf_node_num_cells(void* node) {
+    return node + LEAF_NODE_NUM_CELLS_OFFSET;
+}
+
+void* leaf_node_cell(void* node, uint32_t cell_num) {
+    return node + LEAF_NODE_HEADER_SIZE + cell_num * LEAF_NODE_CELL_SIZE;
+}
+
+uint32_t* leaf_node_key(void* node, uint32_t cell_num) {
+    return leaf_node_cell(node, cell_num);
+}
+
+uint32_t* leaf_node_value(void* node, uint32_t cell_num) {
+    return leaf_node_cell(node, cell_num) + LEAF_NODE_KEY_SIZE;
+}
+
+void initialize_leaf_node(void* node){
+    *leaf_node_num_cells(node) = 0;
+}
+
+// Table Bloc
+
 typedef struct {
     Pager* pager;
     uint32_t root_page_num;
@@ -336,6 +401,7 @@ void deserialize_row(void* source, Row* destination) {
     strncpy(destination->email, source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
+
 // Cursor
 
 typedef struct {
@@ -382,10 +448,32 @@ void cursor_advance(Cursor* cursor) {
    uint32_t page_num = cursor->page_num;
    void* node = get_page(cursor->table->pager, page_num);
 
-   cursor->cell_num =+ 1;
+   cursor->cell_num += 1;
    if (cursor->cell_num >= (*leaf_node_num_cells(node))) {
     cursor->end_of_table = true;
    }
+}
+
+void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value) {
+    void* node = get_page(cursor->table->pager, cursor->page_num);
+
+    uint32_t num_cells = *leaf_node_num_cells(node);
+    if (num_cells >= LEAF_NODE_MAX_CELLS) {
+        // Node full
+        printf("Need to implement splitting a leaf node. \n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (cursor->cell_num < num_cells) {
+        // Make room for new cells
+        for (uint32_t i = num_cells; i > cursor->cell_num; i--) {
+            memcpy(leaf_node_cell(node, i), leaf_node_cell(node, i-1), LEAF_NODE_CELL_SIZE);
+        }
+    }
+
+    *(leaf_node_num_cells(node)) += 1;
+    *(leaf_node_key(node, cursor->cell_num)) = key;
+    serialize_row(value, leaf_node_value(node, cursor->cell_num));
 }
 
 // Meta-command bloc
@@ -454,91 +542,6 @@ ExecuteResult execute_statement(Statement* statement, Table* table) {
         case (STATEMENT_SELECT):
             return execute_select(statement, table);
     }
-}
-
-// Database bloc
-
-typedef enum {
-    NODE_INTERNAL,
-    NODE_LEAF
-} NodeType;
-
-/*
- * Common node Header Layout 
- * 
- * Nodes need to store some metadata in the header at the be=ginning of every page. theis information is common across all nodes.
- * Every node will store the type of node is, weather or not it is the root node, and the address of it parent so that we can find its sibling nodes.
- * The offset caused due to storing this information is calculated and reference below. 
- */
-const uint32_t NODE_TYPE_SIZE = sizeof(uint8_t);
-const uint32_t NODE_TYPE_OFFSET = 0;
-const uint32_t IS_ROOT_SIZE = sizeof(uint8_t);
-const uint32_t IS_ROOT_OFFSET = NODE_TYPE_SIZE;
-const uint32_t PARENT_POINTER_SIZE = sizeof(uint32_t);
-const uint32_t PARENT_POINTER_OFFSET = IS_ROOT_OFFSET + IS_ROOT_SIZE;
-const uint8_t COMMON_NODE_HEADER_SIZE = NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_POINTER_SIZE;
-
-/*
-* Leaf Node Header Layout
-*/
-const uint32_t LEAF_NODE_NUM_CELLS_SIZE = sizeof(uint32_t);
-const uint32_t LEAF_NODE_NUM_CELLS_OFFSET = COMMON_NODE_HEADER_SIZE;
-const uint32_t LEAF_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE + LEAF_NODE_NUM_CELLS_SIZE;
-
-/*
-* Leaf Node Body Layout
-*/
-const uint32_t LEAF_NODE_KEY_SIZE = sizeof(uint32_t);
-const uint32_t LEAF_NODE_KEY_OFFSET = 0;
-const uint32_t LEAF_NODE_VALUE_SIZE = ROW_SIZE;
-const uint32_t LEAF_NODE_VALUE_OFFSET = LEAF_NODE_KEY_OFFSET + LEAF_NODE_KEY_SIZE;
-const uint32_t LEAF_NODE_CELL_SIZE = LEAF_NODE_KEY_SIZE + LEAF_NODE_VALUE_SIZE;
-const uint32_t LEAF_NODE_SPACE_FOR_CELLS = PAGE_SIZE - LEAF_NODE_HEADER_SIZE;
-const uint32_t LEAF_NODE_MAX_CELLS = LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE;
-
-// Accessing leaf node fields
-// functions return a pointer, thus they can be used as both getter and setter
-
-uint32_t* leaf_node_num_cells(void* node) {
-    return node + LEAF_NODE_NUM_CELLS_OFFSET;
-}
-
-void* leaf_node_cell(void* node, uint32_t cell_num) {
-    return node + LEAF_NODE_HEADER_SIZE + cell_num * LEAF_NODE_CELL_SIZE;
-}
-
-uint32_t* leaf_node_key(void* node, uint32_t cell_num) {
-    return leaf_node_cell(node, cell_num);
-}
-
-uint32_t* leaf_node_value(void* node, uint32_t cell_num) {
-    return leaf_node_cell(node, cell_num) + LEAF_NODE_KEY_SIZE;
-}
-
-void initialize_leaf_node(void* node){
-    *leaf_node_num_cells(node) = 0;
-}
-
-void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value) {
-    void* node = get_page(cursor->table->pager, cursor->page_num);
-
-    uint32_t num_cells = *leaf_node_num_cells(node);
-    if (num_cells >= LEAF_NODE_MAX_CELLS) {
-        // Node full
-        printf("Need to implement splitting a leaf node. \n");
-        exit(EXIT_FAILURE);
-    }
-
-    if (cursor->cell_num < num_cells) {
-        // Make room for new cells
-        for (uint32_t i = num_cells; i > cursor->cell_num; i--) {
-            memcpy(leaf_node_cell(node, i), leaf_node_cell(node, i-1), LEAF_NODE_CELL_SIZE);
-        }
-    }
-
-    *(leaf_node_num_cells(node)) += 1;
-    *(leaf_node_key(node, cursor->cell_num)) = key;
-    serialize_row(value, leaf_node_value(node, cursor->cell_num));
 }
 
 
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -140,4 +140,23 @@ describe 'database' do
         reset_result = reset_db()
         expect(reset_result).to eq(0), "Failed to reset the database. Delete operation failed."
     end
+
+    # it 'prints constants' do
+    #     script = [
+    #         ".constants",
+    #         ".exit",
+    #     ]
+    #     result = run_script(script)
+
+    #     expect(result).to match_array([
+    #         "Constants:",
+    #         "ROW_SIZE: 293",
+    #         "COMMON_NODE_HEADER_SIZE: 6",
+    #         "LEAF_NODE_HEADER_SIZE: 10",
+    #         "LEAF_NODE_CELL_SIZE: 297",
+    #         "LEAF_NODE_SPACE_FOR_CELLS: 4086",          
+    #         "LEAF_NODE_MAX_CELLS: 13",
+    #         "db > ",
+    #     ])
+    # end
 end
